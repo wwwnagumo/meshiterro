a13f4bf0023bfaf5703b03c97520ff05fa88c525
wwwnagumo <www.03140624@outlook.jp>
2020-01-23 15:45:38 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '79b94ad2e9aed4566bf3d179bcb326a00f9574ed13d74ae36e0fde4fc43d786097bf938d868defd6af2712da7d53944ae8e8bb60095152215fca191753dca10d'
