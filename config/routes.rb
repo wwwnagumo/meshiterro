c8b8e7969933a29750af9c8dcf9ffb5233aa4320
wwwnagumo <www.03140624@outlook.jp>
2020-01-25 15:33:37 +0900

    [Finish]5-4

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'post_images#index'
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show] 
+  resources :post_images, only: [:new, :create, :index] do
+    resource :post_comments, only:[:create, :destroy]
+  end
+  resources :post_images, only: [:new, :create, :index, :show] do
+  	resource :favorites, only: [:create, :destroy]
+  	resource :post_comments, only: [:create, :destroy]
+  end
 end
