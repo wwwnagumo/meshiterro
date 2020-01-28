03a2a0a5921d12fc3bf7b4a2d7415a26222a56bc
wwwnagumo <www.03140624@outlook.jp>
2020-01-28 18:47:41 +0900

    [Fix]user_controller修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
   def update
-  	@user = User.fond(params[:id])
+  	@user = User.find(params[:id])
   	@user.update(user_params)
   	redirect_to user_path(@user.id)
   end
